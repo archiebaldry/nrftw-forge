b7fe9ca456f0eb3180653c7cde536784c1d5188a
Archie Baldry <33762310+archiebaldry@users.noreply.github.com>
2025-11-15 12:43:02 +0000

    Update gradle properties

--- src/main/java/archiebaldry/nrftw/ExampleMod.java
+++ src/main/java/archiebaldry/nrftw/ExampleMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package archiebaldry.nrftw;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
@@ -33,7 +33,7 @@ import org.slf4j.Logger;
 public class ExampleMod
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "examplemod";
+    public static final String MODID = "nrftw";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
